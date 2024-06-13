c92d9de0fea9b3387dd5f85c7fb959824ab5589a
Mobile-first <aafreenparveen2575@gmail.com>
2024-06-13 10:44:36 +0530

    add images

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("hello");
+    var myCarousel = new bootstrap.Carousel(document.querySelector('#carouselExample'), {
+        interval: 2000,
+        ride: 'carousel',
+        wrap: true
+    })
+});
+
+
 const time = new Date().getFullYear();
 document.getElementById("footer-p").innerText = "©" + time + " Nanhi Kashtiyan | ALL RIGHTS RESERVED";
+
